44ee9316b227d791effbec0c1a12a7c2fb210a39
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-11-22 14:15:19 -0300

    Showing images in ImageScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View
       style={styles.view}
 
     >
       <Text style={styles.text}>Hi there!</Text>
-      <Button
-        style={styles.button}
-        onPress={() => navigation.navigate('Components')}
-        title="Go to Components Demo"
-      />
-      <Button
-        style={styles.button}
-        title="Go to List Demo"
-        onPress={() => navigation.navigate('List')}
-      />
-      <Button
-        style={styles.button}
-        title="Go to List Image"
-        onPress={() => navigation.navigate('Image')}
-      />
+      <View style={styles.viewButton}>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate('Components')}
+          title="Go to Components Demo"
+        />
+      </View>
+      <View style={styles.viewButton}>
+        <Button
+          style={styles.button}
+          title="Go to List Demo"
+          onPress={() => navigation.navigate('List')}
+        />
+      </View>
+      <View style={styles.viewButton}>
+        <Button
+          style={styles.button}
+          title="Go to List Image"
+          onPress={() => navigation.navigate('Image')}
+        />
+      </View>
 
     </View>
   );
@@ -32,14 +38,17 @@ const styles = StyleSheet.create({
   view: {
     margin: 40,
   },
+  viewButton: {
+    marginVertical: 10
+  },
   text: {
     marginBottom: 10,
     fontSize: 30,
     alignSelf: "center"
   },
   button: {
-    paddingTop: 20,
-    marginBottom: 20
+    margin: 10
+
   }
 });
 
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-  return <Text>ImageScreen</Text>;
+  return (
+    <View>
+      <ImageDetail title="Forest" />
+      <ImageDetail title="Beach" />
+      <ImageDetail title="Mountain" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});

4312bc9703afd102e5ef469f38e4d91e69ffe663
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-11-26 14:46:29 -0300

    Finish text input chapter

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [password, setPassword] = useState('');
   return (
     <View>
-      <TextInput style={styles.input} />
+      <Text>Enter Name:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
     </View>
   );
 };
